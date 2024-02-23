106eebd695ea17b6849fe4ae98a8d615ef766fff
89pleasure <133201120+89pleasure@users.noreply.github.com>
2024-02-23 18:50:49 +0100

    some cleanup

--- ExtendedTooltip/Systems/ExtendedTempTooltipSystem.cs
+++ ExtendedTooltip/Systems/ExtendedTempTooltipSystem.cs
@@ -4,7 +4,6 @@ using Game.Prefabs;
 using Game.Simulation;
 using Game.Tools;
 using Game.UI.Tooltip;
-using Gooee.Plugins;
 using HarmonyLib;
 using System;
 using System.Linq;
@@ -25,7 +24,7 @@ namespace ExtendedTooltip.Systems
         private TerrainSystem m_TerrainSystem;
         private TerrainToolSystem m_TerrainToolSystem;
 
-        private LocalSettings m_Settings;
+        private ModSettings m_Settings;
         private StringTooltip m_NetToolMode;
 
         private Type _waterFeaturesType;
@@ -45,7 +44,7 @@ namespace ExtendedTooltip.Systems
             m_ToolRaycastSystem = World.GetOrCreateSystemManaged<ToolRaycastSystem>();
 
             loadedAssemblies = AppDomain.CurrentDomain.GetAssemblies();
-            m_Settings = m_ExtendedTooltipSystem.m_LocalSettings;
+            m_Settings = m_ExtendedTooltipSystem.m_LocalSettings.ModSettings;
             DetectWaterFeaturesMod();
 
             m_NetToolMode = new StringTooltip
@@ -57,54 +56,53 @@ namespace ExtendedTooltip.Systems
         [Preserve]
         protected override void OnUpdate()
         {
-            ModSettings modSettings = m_Settings.ModSettings;
-            if (m_ToolSystem != null && m_ToolSystem.activeTool is not DefaultToolSystem)
+            if (m_ToolSystem == null || m_ToolSystem.activeTool is DefaultToolSystem)
+                return;
+
+            if (m_ToolSystem.activeTool is NetToolSystem && m_Settings.ShowNetToolSystem && m_Settings.ShowNetToolMode)
             {
-                if (m_ToolSystem.activeTool is NetToolSystem && modSettings.ShowNetToolSystem && modSettings.ShowNetToolMode)
+                m_NetToolMode.icon = $"Media/Tools/Net Tool/{m_NetToolSystem.mode}.svg";
+                m_NetToolMode.value = m_CustomTranslationSystem.GetLocalGameTranslation($"ToolOptions.TOOLTIP_TITLE[{m_NetToolSystem.mode}]");
+
+                // Add elevation to tooltip if it's not 0.0f
+                if (m_Settings.ShowNetToolElevation && m_NetToolSystem.elevation != 0.0f)
                 {
-                    m_NetToolMode.icon = $"Media/Tools/Net Tool/{m_NetToolSystem.mode}.svg";
-                    m_NetToolMode.value = m_CustomTranslationSystem.GetLocalGameTranslation($"ToolOptions.TOOLTIP_TITLE[{m_NetToolSystem.mode}]");
-
-                    // Add elevation to tooltip if it's not 0.0f
-                    if (modSettings.ShowNetToolElevation && m_NetToolSystem.elevation != 0.0f)
-                    {
-                        // Add + sign for positive elevation (- is added by default)
-                        string sign = (m_NetToolSystem.elevation > 0.0f) ? "+" : "";
-                        m_NetToolMode.value = m_CustomTranslationSystem.GetLocalGameTranslation($"ToolOptions.TOOLTIP_TITLE[{m_NetToolSystem.mode}]") + $" ({sign}{m_NetToolSystem.elevation} m)";
-                    }
-
-                    AddMouseTooltip(m_NetToolMode);
+                    // Add + sign for positive elevation (- is added by default)
+                    string sign = (m_NetToolSystem.elevation > 0.0f) ? "+" : "";
+                    m_NetToolMode.value = m_CustomTranslationSystem.GetLocalGameTranslation($"ToolOptions.TOOLTIP_TITLE[{m_NetToolSystem.mode}]") + $" ({sign}{m_NetToolSystem.elevation} m)";
                 }
 
-                if ((modSettings.ShowTerrainToolHeight && m_ToolSystem.activeTool is TerrainToolSystem) || (modSettings.ShowWaterToolHeight && (m_ToolSystem.activeTool is WaterToolSystem || AnyOtherSupportedCustomToolSystem())))
+                AddMouseTooltip(m_NetToolMode);
+            }
+
+            if ((m_Settings.ShowTerrainToolHeight && m_ToolSystem.activeTool is TerrainToolSystem) || (m_Settings.ShowWaterToolHeight && (m_ToolSystem.activeTool is WaterToolSystem || AnyOtherSupportedCustomToolSystem())))
+            {
+                TerrainHeightData heightData = m_TerrainSystem.GetHeightData();
+
+                if (m_ToolRaycastSystem.GetRaycastResult(out RaycastResult raycastResult))
                 {
-                    TerrainHeightData heightData = m_TerrainSystem.GetHeightData();
-
-                    if (m_ToolRaycastSystem.GetRaycastResult(out RaycastResult raycastResult))
+                    int applyHeight = (int) TerrainUtils.SampleHeight(ref heightData, raycastResult.m_Hit.m_HitPosition);
+                    StringTooltip terrainToolMode = new()
                     {
-                        int applyHeight = (int) TerrainUtils.SampleHeight(ref heightData, raycastResult.m_Hit.m_HitPosition);
-                        StringTooltip terrainToolMode = new()
-                        {
-                            icon = "Media/Glyphs/Slope.svg",
-                            path = "terrainToolApplyHeight",
-                            value = m_CustomTranslationSystem.GetLocalGameTranslation("Common.VALUE_METER", "-", "SIGN", "", "VALUE", applyHeight.ToString()),
-                        };
-                        AddMouseTooltip(terrainToolMode);
+                        icon = "Media/Glyphs/Slope.svg",
+                        path = "terrainToolApplyHeight",
+                        value = m_CustomTranslationSystem.GetLocalGameTranslation("Common.VALUE_METER", "-", "SIGN", "", "VALUE", applyHeight.ToString()),
+                    };
+                    AddMouseTooltip(terrainToolMode);
 
-                        // Add level height tooltip if the tool is set to level
-                        if (m_ToolSystem.activeTool is TerrainToolSystem && m_TerrainToolSystem.prefab.m_Type == TerraformingType.Level)
+                    // Add level height tooltip if the tool is set to level
+                    if (m_ToolSystem.activeTool is TerrainToolSystem && m_TerrainToolSystem.prefab.m_Type == TerraformingType.Level)
+                    {
+                        float3 targetPosition = Traverse.Create(m_TerrainToolSystem).Field<float3>("m_TargetPosition").Value;
+                        int targetHeight = (int)TerrainUtils.SampleHeight(ref heightData, targetPosition);
+                        StringTooltip terrainTargetPosition = new()
                         {
-                            float3 targetPosition = Traverse.Create(m_TerrainToolSystem).Field<float3>("m_TargetPosition").Value;
-                            int targetHeight = (int)TerrainUtils.SampleHeight(ref heightData, targetPosition);
-                            StringTooltip terrainTargetPosition = new()
-                            {
-                                icon = "Media/Tools/Terrain Tool/Level.svg",
-                                path = "terrainToolTargetHeight",
-                                color = targetHeight > applyHeight ? TooltipColor.Success : targetHeight == applyHeight ? TooltipColor.Info : TooltipColor.Error,
-                                value = m_CustomTranslationSystem.GetLocalGameTranslation("Common.VALUE_METER", "-", "SIGN", "", "VALUE", targetHeight.ToString()),
-                            };
-                            AddMouseTooltip(terrainTargetPosition);
-                        }
+                            icon = "Media/Tools/Terrain Tool/Level.svg",
+                            path = "terrainToolTargetHeight",
+                            color = targetHeight > applyHeight ? TooltipColor.Success : targetHeight == applyHeight ? TooltipColor.Info : TooltipColor.Error,
+                            value = m_CustomTranslationSystem.GetLocalGameTranslation("Common.VALUE_METER", "-", "SIGN", "", "VALUE", targetHeight.ToString()),
+                        };
+                        AddMouseTooltip(terrainTargetPosition);
                     }
                 }
             }
